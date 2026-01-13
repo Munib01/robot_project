97740e3337f5f27887d85b6e438f1bd4af1f2800
Munib01 <munib0ahmed@gmail.com>
2026-01-13 11:22:56 +0000

    :zap: add obstacle avoidance (pre-fined tuned version)

--- src/mobile_robot/mobile_robot/robot_controller.py
+++ src/mobile_robot/mobile_robot/robot_controller.py
@@ -28,6 +28,8 @@ class RobotController(Node):
         self.current_y = 0.0
         self.current_theta = 0.0
         self.min_obstacle_distance = 10.0
+        self.state = "SEEK_GOAL"
+        self.avoidance_start_time = 0.0
         
         # 4. Control Timer (10Hz)
         self.timer = self.create_timer(0.1, self.control_loop)
@@ -48,20 +50,24 @@ class RobotController(Node):
     def lidar_callback(self, msg):
         """Standard LiDAR safety check."""
         if msg.ranges:
-            self.min_obstacle_distance = min(msg.ranges)
+            front_sector = msg.ranges[300:420]
+            clean_ranges = [r if not math.isinf(r) else 10.0 for r in front_sector]
+            self.min_obstacle_distance = min(clean_ranges)
+            
+            # self.min_obstacle_distance = min(msg.ranges)
+            self.get_logger().info(f'min_obstacle_distance: ({self.min_obstacle_distance})')
 
     def control_loop(self):
         msg = Twist()
+        now = self.get_clock().now().seconds_nanoseconds()[0]
 
         # A. Calculate Euclidean distance
         dist_x = self.target_x - self.current_x
         dist_y = self.target_y - self.current_y
         distance_to_goal = math.sqrt(dist_x**2 + dist_y**2)
-        self.get_logger().info(f'current: ({self.current_x}, {self.current_y})')
-        self.get_logger().info(f'target: ({self.target_x}, {self.target_y})')
+        # self.get_logger().info(f'current: ({self.current_x}, {self.current_y})')
+        # self.get_logger().info(f'target: ({self.target_x}, {self.target_y})')
 
-
-        
         # B. Calculate Angle Error
         angle_to_target = math.atan2(dist_y, dist_x)
         angle_error = angle_to_target - self.current_theta
@@ -69,27 +75,40 @@ class RobotController(Node):
         # Normalize angle_error to [-pi, pi] to prevent infinite spinning
         angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
 
-        # C. Safety Logic
-        if self.min_obstacle_distance < 0.6:
-            self.get_logger().warn('Obstacle too close! Emergency Stop.')
+        if distance_to_goal < self.goal_tolerance:
+            self.get_logger().info('SUCCESS: Goal Reached!')
             msg.linear.x = 0.0
-            msg.angular.z = 0.3  # Slowly rotate to clear sensor
-        
-        # D. Navigation Logic
-        else:
-            if distance_to_goal > self.goal_tolerance:
-                # If angle error is large, rotate in place first
-                if abs(angle_error) > 0.3:
-                    msg.linear.x = 0.1  # Creep forward slightly
+            msg.angular.z = 0.0
+            self.publisher_.publish(msg)
+            return  # Exit the loop entirely so it stops moving
+
+        # C. State Machine logic
+        if self.state == "SEEK_GOAL":
+            if self.min_obstacle_distance < 1.2: # Trigger distance
+                self.state = "AVOID_ROTATE"
+            else:
+                if abs(angle_error) > 0.2:
                     msg.angular.z = 0.6 if angle_error > 0 else -0.6
                 else:
-                    # Pointing correctly, move forward
-                    msg.linear.x = 0.6
-                    msg.angular.z = 0.0
+                    msg.linear.x = 0.5
+        
+        elif self.state == "AVOID_ROTATE":
+            self.get_logger().warn('ROTATING TO CLEAR...')
+            # Rotate until front is very clear
+            if self.min_obstacle_distance < 2.5:
+                msg.angular.z = 0.7 
             else:
-                self.get_logger().info('SUCCESS: Goal Reached!')
-                msg.linear.x = 0.0
+                self.state = "AVOID_DRIVE"
+                self.avoidance_start_time = now
+                
+        elif self.state == "AVOID_DRIVE":
+            self.get_logger().info('DRIVING PAST OBSTACLE...')
+            # Drive for 5 seconds (as you set) to fully clear the wall
+            if (now - self.avoidance_start_time) < 5.0:
+                msg.linear.x = 0.4
                 msg.angular.z = 0.0
+            else:
+                self.state = "SEEK_GOAL"
 
         self.publisher_.publish(msg)
 
